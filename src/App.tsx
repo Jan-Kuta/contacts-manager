9cd01b85a4c35fa84497eb7bb28a26c1f1eae48c
Jan Kuta <jan.kuta@email.cz>
2023-09-08 15:39:57 +0200

    First component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Contact } from './components/Contact'
 
 function App() {
   return (
     <div>
       <h1>Contacts manager</h1>
+      <Contact />
     </div>
   )
 }
---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+
+export const Contact = () => {
+  return (
+    <div>
+      <h4>Jan Kuta</h4>
+      <ul>
+        <li>Email: jan.kuta@email.cz</li>
+        <li>Phone: +420 123 456 789</li>
+      </ul>
+    </div>
+  )
+}
