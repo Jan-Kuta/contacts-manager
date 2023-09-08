a644e34881a4b599a670ef0f5c6ee23eb51318e0
Jan Kuta <jan.kuta@email.cz>
2023-09-08 17:59:42 +0200

    Set default sizes for headings

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import './App.css'
 function App() {
   return (
     <div>
-      <button className="btn btn-primary">Button</button>
       <Heading text="Contacts manager" />
       <Contact name="Jan Kuta" email="jan.kuta@email.cz" phone="+420 123 456 789" />
       <Contact name="Jana Kutová" email="jana.kutova@email.cz" phone="+420 987 654 321" />
