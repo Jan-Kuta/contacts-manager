f7e0c0f7fab612c9e5164e720e6ba403e5f03f00
Jan Kuta <jan.kuta@email.cz>
2023-09-08 17:25:55 +0200

    Add tailwind css library

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h4>App component</h4>
+      <div className="text-3xl font-bold underline">App component</div>
       <Heading text="Contacts manager" />
       <Contact name="Jan Kuta" email="jan.kuta@email.cz" phone="+420 123 456 789" />
       <Contact name="Jana Kutová" email="jana.kutova@email.cz" phone="+420 987 654 321" />
