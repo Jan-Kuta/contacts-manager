7d82b9f10f861e30bda1b341fad129e4b139f5c7
Jan Kuta <jan.kuta@email.cz>
2023-09-09 09:02:48 +0200

    Event handling - clickEvent

--- src/components/Contact/ConatctInfoLine.tsx
+++ src/components/Contact/ConatctInfoLine.tsx
@@ -5,8 +5,19 @@ type Props = {
 }
 
 export const ConatctInfoLine = ({ value, icon }: Props) => {
+  // coppy value to clipboard
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(value)
+      .then(() => {
+        alert('Value copied to clipboard')
+      })
+      .catch(() => {
+        alert('Value failed to copy to clipboard')
+      })
+  }
+
   return (
-    <button type="button"
+    <button type="button" onClick={copyToClipboard}
             className="relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b border-gray-200 rounded-t-lg hover:bg-gray-100 hover:text-blue-700">
       {icon}
       {value}
